6eeb2bc74b9ac70149f339215921589136cb0f39
Nishan Bista <nishanbista525@gmail.com>
2024-04-16 11:55:25 +0545

    fixed:time

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,17 @@ import React, { useEffect, useState } from "react";
 function Footer() {
   const [date, setDate] = useState(new Date());
 
-  let time = date.toLocaleTimeString();
+  const options = {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true
+  };
+
+
+  // 01:00:50 AM
+
+  let time = date.toLocaleTimeString(options);
   console.log(time.length);
 
   const [extra, setExtra] = useState("");
@@ -11,17 +21,17 @@ function Footer() {
   setInterval(() => {
     setDate(new Date());
     if (
-      (time.slice(8, 10) == "AM" && Number(time.slice(0, 1)) >= 12) ||
-      Number(time.slice(0, 1)) <= 4
+      (time.slice(9, 11) == "AM" && Number(time.slice(0, 2)) >= 12) &&
+      Number(time.slice(0, 2)) <= 4
     ) {
       setExtra("Its time to sleep, Are you an OWL?");
     } else if (
-      (time.slice(8, 10) == "AM" && Number(time.slice(0, 1)) >= 5) ||
-      Number(time.slice(0, 1)) < 9
+      (time.slice(9, 11) == "AM" && Number(time.slice(0, 2)) >= 5) &&
+      Number(time.slice(0, 2)) < 9
     ) {
       setExtra("Good Morning!");
     } else if (
-      (time.slice(9, 11) == "PM" && Number(time.slice(0, 2)) >= 10) ||
+      (time.slice(9, 11) == "PM" && Number(time.slice(0, 2)) >= 10) &&
       Number(time.slice(0, 2)) < 12
     ) {
       setExtra("Goodnight, Sweet Dreams!");
