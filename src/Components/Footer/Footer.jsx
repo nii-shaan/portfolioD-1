872fcfb662583f950627d5156ab9401ced03ed8a
Nishan Bista <nishanbista525@gmail.com>
2024-04-17 22:14:57 +0545

    removed:quotes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -11,31 +11,11 @@ function Footer() {
   };
 
 
-  // 01:00:50 AM
 
   let time = date.toLocaleTimeString(options);
-  console.log(time.length);
-
-  const [extra, setExtra] = useState("");
 
   setInterval(() => {
     setDate(new Date());
-    if (
-      (time.slice(9, 11) == "AM" && Number(time.slice(0, 2)) >= 12) &&
-      Number(time.slice(0, 2)) <= 4
-    ) {
-      setExtra("Its time to sleep, Are you an OWL?");
-    } else if (
-      (time.slice(9, 11) == "AM" && Number(time.slice(0, 2)) >= 5) &&
-      Number(time.slice(0, 2)) < 9
-    ) {
-      setExtra("Good Morning!");
-    } else if (
-      (time.slice(9, 11) == "PM" && Number(time.slice(0, 2)) >= 10) &&
-      Number(time.slice(0, 2)) < 12
-    ) {
-      setExtra("Goodnight, Sweet Dreams!");
-    } else setExtra("");
   }, 1000);
 
   //  console.log(time.slice(0,2));
